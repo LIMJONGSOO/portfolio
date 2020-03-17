5120ef238280af6ad46c45d62d85d82535b5d022
LIMJONGSOO <limjs4535@gmail.com>
2020-03-17 15:06:18 +0900

    route modify

---
+++ src/components/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navigator from './Navigator';
+import Experience from './Experience';
+import Main from './Main';
+import AboutMe from './AboutMe';
+import Education from './Education';
+import Projects from './Projects';
+import Skills from './Skils';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <>
+      <Navigator/>
+      <Main/>
+      <AboutMe/>
+      <Experience/>
+      <Education/>
+      <Projects/>
+      <Skills/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Main.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <>
+        <div id="lead">
+            <div id="lead-content">
+                <h1>Ryan Fitzgerald</h1>
+                <h2>Software Engineer</h2>
+                <a href="#" className="btn-rounded-white">Download Resume</a>
+            </div>
+
+            <div id="lead-overlay"></div>
+
+            <div id="lead-down">
+                <span>
+                    <i className="fa fa-chevron-down" aria-hidden="true"></i>
+                </span>
+            </div>
+        </div>
+    </>
+  );
+}
+
+export default Main;
