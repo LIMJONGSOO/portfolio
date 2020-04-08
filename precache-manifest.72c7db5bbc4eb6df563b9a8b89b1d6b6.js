fb800647614b9a2f3bf4a576741372dc8a07da06
LIMJONGSOO <limjs4535@gmail.com>
2020-04-08 23:55:39 +0900

    Updates

---
+++ precache-manifest.72c7db5bbc4eb6df563b9a8b89b1d6b6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38eae7a8e22450619754cad3760be28",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "433b3fb18783967d54c3",
+    "url": "/portfolio/static/css/main.7415aa27.chunk.css"
+  },
+  {
+    "revision": "5d8df7ef4fc300ea463f",
+    "url": "/portfolio/static/js/2.d7e54821.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.d7e54821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "433b3fb18783967d54c3",
+    "url": "/portfolio/static/js/main.3eab789d.chunk.js"
+  },
+  {
+    "revision": "aaa784e25c10c4ba645c",
+    "url": "/portfolio/static/js/runtime-main.603eb588.js"
+  },
+  {
+    "revision": "7583a74e8e8b97aa450c38b515c6a0bf",
+    "url": "/portfolio/static/media/CSS.7583a74e.png"
+  },
+  {
+    "revision": "a056e21f574fa0e0e40579984ce5d858",
+    "url": "/portfolio/static/media/JAVA.a056e21f.png"
+  },
+  {
+    "revision": "9075d3dea0046f5de45bb157ba7fdb30",
+    "url": "/portfolio/static/media/JSP.9075d3de.png"
+  },
+  {
+    "revision": "bf67ab46b86bd8cff6498c2433d52ab8",
+    "url": "/portfolio/static/media/Phonegap.bf67ab46.png"
+  },
+  {
+    "revision": "0d7be59751e42e81eeb004908e8442a6",
+    "url": "/portfolio/static/media/React.0d7be597.png"
+  },
+  {
+    "revision": "6d7af5662de0e3ded7eff9d092af3ec2",
+    "url": "/portfolio/static/media/Spring.6d7af566.png"
+  },
+  {
+    "revision": "c2640be55af06c7c2ec931ab51fc5d74",
+    "url": "/portfolio/static/media/android.c2640be5.png"
+  },
+  {
+    "revision": "52ddce0a2352cf2fb80e091e560cbd92",
+    "url": "/portfolio/static/media/git.52ddce0a.png"
+  },
+  {
+    "revision": "de736194fc667ffe4958247ad87bbe02",
+    "url": "/portfolio/static/media/index.6c2104b8.css"
+  },
+  {
+    "revision": "6bf3abacee0a691858c97b1302ebe286",
+    "url": "/portfolio/static/media/jQuery.6bf3abac.png"
+  },
+  {
+    "revision": "1932e87f8af96b0b2d5bcde5ccd0a07a",
+    "url": "/portfolio/static/media/js.1932e87f.png"
+  },
+  {
+    "revision": "bef677b3a01f82d23b786a2341b82027",
+    "url": "/portfolio/static/media/less.bef677b3.png"
+  },
+  {
+    "revision": "e9ab50e3d0d5f92accb673e7869776c3",
+    "url": "/portfolio/static/media/limjs.e9ab50e3.jpg"
+  },
+  {
+    "revision": "43b9f4a0a49c0cf24b9b28283b6fe3d2",
+    "url": "/portfolio/static/media/logo.43b9f4a0.png"
+  },
+  {
+    "revision": "3d33f90f31f86b8fe2192ab269ff76d2",
+    "url": "/portfolio/static/media/maven.3d33f90f.png"
+  },
+  {
+    "revision": "4058474a26ee2160dd7669967357bb39",
+    "url": "/portfolio/static/media/oracle.4058474a.png"
+  }
+]);
